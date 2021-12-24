742c9f3d6a3849a9d4db7a2c209e959626839e40
nika-gelashvili <nika.gelash@gmail.com>
2021-12-24 21:11:47 +0400

    Implement getRandom function into SportFacility model to get single random sport facility from sport_facilities table and implement relation to cards model.

--- app/Models/SportFacility.php
+++ app/Models/SportFacility.php
@@ -10,4 +10,21 @@ class SportFacility extends Model
 {
     protected $table = 'sport_facilities';
     use HasFactory, Uuids;
+
+    /**
+     * Get single random sport facility
+     * @return mixed
+     */
+    public static function getRandom()
+    {
+        return self::inRandomOrder()->first();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function card()
+    {
+        return $this->hasMany(Card::class);
+    }
 }
