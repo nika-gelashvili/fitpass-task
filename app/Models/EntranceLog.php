27bff4a6866381fe3d604a890b69ff9726f50f99
nika-gelashvili <nika.gelash@gmail.com>
2021-12-24 21:18:12 +0400

    Implement EntranceLog model for table entrance_logs ans integrate Uuids tait.

---
+++ app/Models/EntranceLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EntranceLog extends Model
+{
+    use HasFactory, Uuids;
+}
