59938ff64947e021b28cf4cc9e0eef78a92ccf15
nika-gelashvili <nika.gelash@gmail.com>
2021-12-24 21:23:29 +0400

    Implement relation EntranceLog model to SportFacility and User model.

--- app/Models/EntranceLog.php
+++ app/Models/EntranceLog.php
@@ -10,4 +10,21 @@ use Illuminate\Database\Eloquent\Model;
 class EntranceLog extends Model
 {
     use HasFactory, Uuids;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function facility()
+    {
+        return $this->belongsTo(SportFacility::class, 'sport_facility_id', 'id');
+    }
 }
