7a50616101ad56a7ba8287687bf3d3a420c64104
nika-gelashvili <nika.gelash@gmail.com>
2021-12-24 21:27:09 +0400

    Implement seeders for models into DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+        $this->call(SportFacilitySeeder::class);
+        $this->call(CardSeeder::class);
+        $this->call(EntranceLogSeeder::class);
     }
 }
