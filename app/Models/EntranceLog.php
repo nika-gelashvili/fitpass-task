f36c2f9c473c4b0f7625626e257945921503bd73
nika-gelashvili <nika.gelash@gmail.com>
2021-12-25 21:52:57 +0400

    Implement checkUserHasEnteredFacility and saveEntrance functions into EntranceLog model.

--- app/Models/EntranceLog.php
+++ app/Models/EntranceLog.php
@@ -12,6 +12,35 @@ class EntranceLog extends Model
     use HasFactory, Uuids;
 
     /**
+     * Function checks if there are any entrance logs for today by $userId and $facilityId
+     * @param $userId
+     * @param $facilityId
+     * @return mixed
+     */
+    public static function checkUserHasEnteredFacility($userId, $facilityId)
+    {
+        return self::where('user_id', $userId)
+            ->where('sport_facility_id', $facilityId)
+            ->whereDate('created_at', '=', Carbon::today()->toDateString())
+            ->first();
+    }
+
+    /**
+     * Function saves entrance logs
+     * @param $userId
+     * @param $facilityId
+     * @return bool
+     */
+    public static function saveEntrance($userId, $facilityId)
+    {
+        $entranceLog = new EntranceLog();
+        $entranceLog->user_id = $userId;
+        $entranceLog->sport_facility_id = $facilityId;
+        $entranceLog->created_at = Carbon::today()->toDateString();
+        return $entranceLog->save();
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user()
