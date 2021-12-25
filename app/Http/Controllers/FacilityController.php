bb4849da508b35981f4381523599dc30f26b38ec
nika-gelashvili <nika.gelash@gmail.com>
2021-12-25 21:45:01 +0400

    Implement FacilityController controller and integrate ApiResponser trait.

---
+++ app/Http/Controllers/FacilityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use App\Models\EntranceLog;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+class FacilityController extends Controller
+{
+    use ApiResponser;
+}
